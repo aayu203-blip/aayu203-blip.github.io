16c4e44c8aa2d6ca7874ccf5a33d2e2bad6a41de
aayu203-blip <aayu203@gmail.com>
2026-02-02 11:57:32 +0530

    fix(seo): update robots.txt sitemap url

--- god-mode/app/robots.ts
+++ god-mode/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: ['/api/', '/admin/'],
         },
-        sitemap: 'https://nexgen-index.com/sitemap.xml',
+        sitemap: 'https://www.nexgenspares.com/sitemap.xml',
     };
 }
